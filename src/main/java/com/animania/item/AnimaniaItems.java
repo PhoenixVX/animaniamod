dfa1ba20c5943389e6ad72878f4047d58c5933f6
Zero_DSRS_VX <24632178+PhoenixVX@users.noreply.github.com>
2024-11-08 16:54:44 -0700

    Add all items to creative inventory + fix data runs

--- src/main/java/com/animania/item/AnimaniaItems.java
+++ src/main/java/com/animania/item/AnimaniaItems.java
@@ -16,11 +16,17 @@ public class AnimaniaItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(AnimaniaMod.MOD_ID);
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(BuiltInRegistries.CREATIVE_MODE_TAB, AnimaniaMod.MOD_ID);
 
-    // Animania base items
+    // Animania base block items
     public static final Supplier<Item> MUD_BLOCK_ITEM = registerBlockItem("mud_block", AnimaniaBlocks.MUD_BLOCK, new Item.Properties());
     public static final Supplier<Item> STRAW_BLOCK_ITEM = registerBlockItem("straw_block", AnimaniaBlocks.STRAW_BLOCK, new Item.Properties());
     public static final Supplier<Item> SALT_LICK_BLOCK_ITEM = registerBlockItem("salt_lick_block", AnimaniaBlocks.SALT_LICK_BLOCK, new Item.Properties());
+    public static final Supplier<Item> TROUGH_BLOCK_ITEM = registerItem("trough_block", () -> new Item(new Item.Properties().stacksTo(1)));
+    public static final Supplier<Item> NEST_BLOCK_ITEM = registerItem("nest_block", () -> new Item(new Item.Properties().stacksTo(1)));
+
+    // Animania base items
     public static final Supplier<Item> SLOP_BUCKET = registerItem("slop_bucket", () -> new BucketItem(AnimaniaFluids.SLOP_FLUID.value(), new Item.Properties().craftRemainder(Items.BUCKET).stacksTo(1)));
+    public static final Supplier<Item> MANUAL = registerItem("manual", () -> new Item(new Item.Properties().stacksTo(1)));
+    public static final Supplier<Item> RANDOM_SPAWN_EGG = registerItem("random_spawn_egg", () -> new Item(new Item.Properties().stacksTo(1)));
 
     // Animania item groups
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> ANIMANIA_ENTITIES = CREATIVE_MODE_TABS.register("animania_entities", () ->
@@ -28,15 +34,22 @@ public class AnimaniaItems {
                     .title(Component.translatable("itemGroup.animania_entities"))
                     .icon(() -> new ItemStack(Items.EGG))
                     .displayItems(((parameters, output) -> {
-                        output.accept(new ItemStack(MUD_BLOCK_ITEM.get()));
+                        output.accept(new ItemStack(RANDOM_SPAWN_EGG.get()));
                     }))
                     .build()
     );
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> ANIMANIA_RESOURCES = CREATIVE_MODE_TABS.register("animania_resources", () ->
             CreativeModeTab.builder()
                     .title(Component.translatable("itemGroup.animania_resources"))
+                    .icon(() -> new ItemStack(SLOP_BUCKET.get()))
                     .displayItems(((parameters, output) -> {
+                        output.accept(new ItemStack(SLOP_BUCKET.get()));
+                        output.accept(new ItemStack(MANUAL.get()));
+                        output.accept(new ItemStack(STRAW_BLOCK_ITEM.get()));
+                        output.accept(new ItemStack(SALT_LICK_BLOCK_ITEM.get()));
                         output.accept(new ItemStack(MUD_BLOCK_ITEM.get()));
+                        output.accept(new ItemStack(TROUGH_BLOCK_ITEM.get()));
+                        output.accept(new ItemStack(NEST_BLOCK_ITEM.get()));
                     }))
                     .build()
     );
