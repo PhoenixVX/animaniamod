4b0689710251909c81f6b15f7849269a471a7cda
Zero_DSRS_VX <24632178+PhoenixVX@users.noreply.github.com>
2024-11-09 00:28:43 -0700

    Trough container impl

--- src/main/java/com/animania/block/entity/TroughBlockEntity.java
+++ src/main/java/com/animania/block/entity/TroughBlockEntity.java
@@ -4,14 +4,16 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.Container;
 import net.minecraft.world.ContainerHelper;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 
-public class TroughBlockEntity extends BlockEntity {
-    private NonNullList<ItemStack> items = NonNullList.withSize(this.getInventorySize(), ItemStack.EMPTY);
+public class TroughBlockEntity extends BlockEntity implements Container {
+    private NonNullList<ItemStack> items = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
 
     public TroughBlockEntity(BlockPos pos, BlockState blockState) {
         super(AnimaniaBlockEntityTypes.TROUGH_BLOCK_ENTITY.get(), pos, blockState);
@@ -20,7 +22,7 @@ public class TroughBlockEntity extends BlockEntity {
     @Override
     protected void loadAdditional(@NotNull CompoundTag tag, HolderLookup.@NotNull Provider registries) {
         super.loadAdditional(tag, registries);
-        this.items = NonNullList.withSize(this.getInventorySize(), ItemStack.EMPTY);
+        this.items = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
         ContainerHelper.loadAllItems(tag, this.items, registries);
     }
 
@@ -30,8 +32,43 @@ public class TroughBlockEntity extends BlockEntity {
         ContainerHelper.saveAllItems(tag, this.items, registries);
     }
 
-    // Utility methods
-    public int getInventorySize() {
+    @Override
+    public int getContainerSize() {
         return 3;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.items.stream().allMatch(ItemStack::isEmpty);
+    }
+
+    @Override
+    public @NotNull ItemStack getItem(int slot) {
+        return this.items.get(slot);
+    }
+
+    @Override
+    public @NotNull ItemStack removeItem(int slot, int amount) {
+        return ContainerHelper.removeItem(this.items, slot, amount);
+    }
+
+    @Override
+    public @NotNull ItemStack removeItemNoUpdate(int slot) {
+        return ContainerHelper.takeItem(this.items, slot);
+    }
+
+    @Override
+    public void setItem(int slot, @NotNull ItemStack stack) {
+        this.items.set(slot, stack);
+    }
+
+    @Override
+    public boolean stillValid(@NotNull Player player) {
+        return true;
+    }
+
+    @Override
+    public void clearContent() {
+        this.items = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
     }
 }
