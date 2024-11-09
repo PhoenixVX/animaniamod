7fdcabb2153c510d4df7eeac942d6fd0f5abc466
Zero_DSRS_VX <24632178+PhoenixVX@users.noreply.github.com>
2024-11-09 01:07:25 -0700

    Fix slop liquid properties

--- src/main/java/com/animania/block/AnimaniaBlocks.java
+++ src/main/java/com/animania/block/AnimaniaBlocks.java
@@ -5,6 +5,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.PushReaction;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.Function;
@@ -17,7 +18,7 @@ public class AnimaniaBlocks {
     public static final Supplier<Block> MUD_BLOCK = registerBlock("mud_block", MudBlock::new, BlockBehaviour.Properties.of().strength(1.0F, 1.0F).speedFactor(0.6F).sound(SoundType.SLIME_BLOCK).randomTicks());
     public static final Supplier<Block> STRAW_BLOCK = registerBlock("straw_block", StrawBlock::new, BlockBehaviour.Properties.of().sound(SoundType.GRASS).noOcclusion());
     public static final Supplier<Block> SALT_LICK_BLOCK = registerBlock("salt_lick_block", SaltLickBlock::new, BlockBehaviour.Properties.of().strength(1.2F, 1.7F).noOcclusion());
-    public static final Supplier<LiquidBlock> SLOP_BLOCK = registerBlock("slop_block", SlopBlock::new, BlockBehaviour.Properties.of().strength(100.0F));
+    public static final Supplier<LiquidBlock> SLOP_BLOCK = registerBlock("slop_block", SlopBlock::new, BlockBehaviour.Properties.of().strength(100.0F).replaceable().liquid().pushReaction(PushReaction.DESTROY).noLootTable());
     public static final Supplier<Block> TROUGH_BLOCK = registerBlock("trough_block", TroughBlock::new, BlockBehaviour.Properties.of());
     public static final Supplier<Block> NEST_BLOCK = registerBlock("nest_block", NestBlock::new, BlockBehaviour.Properties.of().noOcclusion());
 
